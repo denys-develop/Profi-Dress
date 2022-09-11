0234352a9e4dc2c054201b2ad53b5f513781ef2e
denys-develop <denys.tsuprenko@yandex.ru>
2022-09-11 22:30:26 +0300

    add modal-order

--- js/script.js
+++ js/script.js
@@ -16,3 +16,45 @@ $('.owl-prev').click(function() {
 $('.owl-next').click(function() {
     owl.trigger('next.owl.carousel');
 })
+
+
+// Modal
+const modalButtons = document.querySelectorAll('[data-modal-button]');
+const modalCloseButtons = document.querySelectorAll('[data-modal-close]');
+const allModals = document.querySelectorAll('[data-modal]');
+const body = document.body;
+
+// Кнопки открыть модалку
+modalButtons.forEach(function(item) {
+    item.addEventListener('click', function() {
+        const modalId =  this.dataset.modalButton;
+        const modal = document.querySelector('#' + modalId);
+        modal.classList.remove('hide');
+        body.classList.add('no-scroll');
+
+        // Находим внутри открываемой модалки блок .modal-window и запрещаем ему передавать клики "наверх"
+        modal.querySelector('.modal-window').addEventListener('click', function (event) {
+            event.stopPropagation();
+        });
+        
+     });
+});
+
+// Кнопки закрыть модалку
+modalCloseButtons.forEach(function(item) {
+    item.addEventListener('click', function() {
+        const modal = this.closest('[data-modal]'); 
+        modal.classList.add('hide');
+    });
+    
+});
+
+// Закрытие модалок по фейду .fade-block
+allModals.forEach(function(item) {
+    item.addEventListener('click', function() {
+      this.classList.add('hide');
+      body.classList.remove('no-scroll');
+    });
+});
+
+
